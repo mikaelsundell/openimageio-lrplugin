8b2b47e3c6fe0b4da874d06f3934adc7dba096ea
Mikael Sundell <mikael.sundell@gmail.com>
2022-10-31 22:57:54 +0100

    Removed dump function

--- openimageio.lrplugin/OpenImageIOFilterProvider.lua
+++ openimageio.lrplugin/OpenImageIOFilterProvider.lua
@@ -360,21 +360,6 @@ function OpenImageIOFilterProvider.sectionForFilterInDialog( viewFactory, proper
 	}
 end
 
-
-function dump(o)
-	if type(o) == 'table' then
-	   local s = '{ '
-	   for k,v in pairs(o) do
-		  if type(k) ~= 'number' then k = '"'..k..'"' end
-		  s = s .. '['..k..'] = ' .. dump(v) .. ','
-	   end
-	   return s .. '} '
-	else
-	   return tostring(o)
-	end
- end
-
-
 function OpenImageIOFilterProvider.postProcessRenderedPhotos(functionContext, filterContext )
 
 	LogInfo("Starting post process rendering")
